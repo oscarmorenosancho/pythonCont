45ecc463d7c70d08b5546c402725770f0cf30f2b
Oscar Moreno Sancho <oscar.moreno.sancho@gmail.com>
2024-03-19 14:07:08 +0100

    now channels work with async consumer that can use async func

--- project/docu/consumers.py
+++ project/docu/consumers.py
@@ -1,38 +1,42 @@
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
-class JoinAndLeave(WebsocketConsumer):
-    # async
-    def connect(self):
-        self.accept()
+class JoinAndLeave(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
         print("server says connected")
         self.group_name = 'alltoghether'
-        # await self.channel_layer.group_add(
-        #     self.group_name,
-        #     self.channel_name
-        # )
-        # self.channel_layer.group_send(
-        #         self.group_name,
-        #         {
-        #             'type': 'channel_msg',
-        #             'msg': "Server sends Welcome to channel"
-        #         }
-        #     )
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
+        await self.channel_layer.group_send(
+                self.group_name,
+                {
+                    'type': 'channel_msg',
+                    'msg': "Server sends Welcome to channel"
+                }
+            )
 
-    # async
-    def receive(self, text_data=None, bytes_data=None):
+    async def receive(self, text_data=None, bytes_data=None):
         print("server says client message received: ", text_data)
 
-    # async
-    # def channel_msg(self, event):
-    #     msg = event['msg']
-    #     await self.send(text_data = json.dumps ({
-    #         'msg': msg 
-    #     }))
+    async def channel_msg(self, event):
+        msg = event['msg']
+        await self.send(text_data = json.dumps ({
+            'msg': msg 
+        }))
 
-    # async
-    def disconnect(self, code):
-        # await self.channel_layer.group_discard(
-        #     self.group_name,
-        # )
+    async def disconnect(self, code):
+        await self.channel_layer.group_send(
+                self.group_name,
+                {
+                    'type': 'channel_msg',
+                    'msg': "Server sends Goodbye to channel"
+                }
+            )
+        await self.channel_layer.group_discard(
+            self.group_name,
+            'alltoghether'
+        )
         print("server says disconnected")
