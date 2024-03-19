70ed40d1527161e86a3f1e21b0a74259314c6c9b
Oscar Moreno Sancho <oscar.moreno.sancho@gmail.com>
2024-03-19 10:57:43 +0100

    adding channels layers

--- project/docu/consumers.py
+++ project/docu/consumers.py
@@ -1,13 +1,34 @@
 from channels.generic.websocket import WebsocketConsumer
+import json
 
 class JoinAndLeave(WebsocketConsumer):
-    def connect(self):
+    async def connect(self):
         print("server says connected")
+        self.group_name = 'alltoghether'
+        # await self.channel_layer.group_add(
+        #     self.group_name,
+        #     self.channel_name
+        # )
         self.accept()
+        # self.channel_layer.group_send(
+        #         self.group_name,
+        #         {
+        #             'type': 'channel_msg',
+        #             'msg': "Server sends Welcome to channel"
+        #         }
+        #     )
 
-    def receive(self, text_data=None, bytes_data=None):
+    async def receive(self, text_data=None, bytes_data=None):
         print("server says client message received: ", text_data)
-        self.send("Server sends Welcome")
-    
-    def disconnect(self, code):
+
+    async def channel_msg(self, event):
+        msg = event['msg']
+        await self.send(text_data = json.dumps ({
+            'msg': msg 
+        }))
+
+    async def disconnect(self, code):
+        # await self.channel_layer.group_discard(
+        #     self.group_name,
+        # )
         print("server says disconnected")
